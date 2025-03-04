ede5fea2cce159778869a47d75ac3cce3a220ac3
Luke Kiprop <luke.rono@student.moringaschool.com>
2025-03-04 12:00:50 +0300

    Add files

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+// Define num1 and num2 for multiply function
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Define random function
+const random = Math.floor(Math.random() * 100) + 0;
+
+// Define num3 and num4 for mod function
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// Define max function
+const max = Math.max(1, 5, 10, 15, 20);
